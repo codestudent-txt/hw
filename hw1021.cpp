add45eb443cae0c146a72331c16bcb97d08fe381
fookila <llsligo@gmail.com>
2024-10-21 16:13:06 +0800

    first commit

---
+++ hw1021.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int A(int m, int n) {
+    if (m == 0) {
+        return n + 1;  
+        return A(m - 1, 1);  
+    } else {
+        return A(m - 1, A(m, n - 1));  
+    }
+}
+
+int main() {
+    cout << "input m, n: ";
+    int m, n;
+    cin >> m >> n;
+    
+    int result = A(m, n);  
+    cout << "A(" << m << "," << n << ") = " << result << endl;  
+    return 0;
+}
